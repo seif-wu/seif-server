d9adf01a27a98b20da0d6c3a08f7d8d53e7ee059
Seif Wu <seif.wuxf@gmail.com>
2022-04-03 23:55:49 +0800

    feat: themoivedb 只代理 GET 请求

--- app/controllers/api/tmdb.py
+++ app/controllers/api/tmdb.py
@@ -5,7 +5,7 @@ from flask import Blueprint, jsonify, request
 tmdb_bp = Blueprint('tmdb', __name__, url_prefix='/tmdb')
 
 
-@tmdb_bp.route('/<path:url>', methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+@tmdb_bp.get('/<path:url>')
 def proxy(url):
     base_url = os.getenv("TMDB_V3_API")
 
@@ -13,8 +13,7 @@ def proxy(url):
         "utf8") + "&api_key=" + os.getenv("TMDB_V3_API_KEY") + "&language=zh-CN"
     url = "{}/{}?{}".format(base_url, url, query)
     method = request.method
-    json_body = request.get_json()
 
-    resp = requests.request(method, url, json=json_body)
+    resp = requests.request(method, url)
 
     return jsonify(resp.json())
