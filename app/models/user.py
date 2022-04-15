5832b91f3e77197b5c4f4093240bd76f8802aeff
Seif Wu <seif.wuxf@gmail.com>
2022-04-15 17:01:35 +0800

    feat: 添加头像字段

--- app/models/user.py
+++ app/models/user.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    avatar = db.Column(db.String(255))
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
