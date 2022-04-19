635285443a61aff14facb49fc9e8c92fd1bbc97a
Seif Wu <seif.wuxf@gmail.com>
2022-04-19 15:25:34 +0800

    feat: 添加 WeChat User 表

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
 class Models:
     @staticmethod
     def init_app(app):
-        from . import user, wechat_mini_program
+        from . import user, wechat_mini_program, wechat_user
---
+++ app/models/wechat_user.py
@@ -0,0 +1,16 @@
+from app import db
+
+
+class WechatUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    mobile = db.Column(db.String(20), nullable=False)
+    avatar_url = db.Column(db.String(255))
+    wechat_userid = db.Column(db.String(255))
+    wechat_openid = db.Column(db.String(255))
+    wechat_unionid = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship("User", back_populates="user")
+
+    def __repr__(self):
+        return '<WechatUser %r>' % self.name
