861445f5f64877029c4ac650b2d416d292dc8adb
Seif Wu <seif.wuxf@gmail.com>
2022-04-27 16:53:45 +0800

    feat: 新增"我想看"创建、删除接口

--- app/controllers/api/v1/weapp/__init__.py
+++ app/controllers/api/v1/weapp/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 from .auth import weapp_auth_bp
 from .user import weapp_user_bp
 from .watched import weapp_watched_bp
+from .want_watch import weapp_want_watched_bp
 
 
 weapp_bp = Blueprint('weapp', __name__, url_prefix='/weapp')
@@ -11,3 +12,4 @@ weapp_bp = Blueprint('weapp', __name__, url_prefix='/weapp')
 weapp_bp.register_blueprint(weapp_auth_bp)
 weapp_bp.register_blueprint(weapp_user_bp)
 weapp_bp.register_blueprint(weapp_watched_bp)
+weapp_bp.register_blueprint(weapp_want_watched_bp)
---
+++ app/controllers/api/v1/weapp/want_watch.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+
+from app import db
+from app.models.schema.want_watch_schema import WantWatchSchema
+from app.models.want_watch import WantWatch
+
+weapp_want_watched_bp = Blueprint(
+    'want_watch', __name__, url_prefix='/want_watch')
+
+
+@weapp_want_watched_bp.post('')
+@jwt_required()
+def create():
+    current_user_id = get_jwt_identity()
+
+    w = WantWatch.query.filter_by(tmdb_id=request.json.get(
+        "tmdb_id"), user_id=current_user_id).first()
+    if w is not None:
+        return jsonify(
+            success=False,
+            message="已存在于列表中"
+        ), 400
+
+    want_watch = WantWatch(
+        title=request.json.get("title"),
+        tmdb_id=request.json.get("tmdb_id"),
+        poster_url=request.json.get("poster_url"),
+        backdrop_path=request.json.get("backdrop_path"),
+        media_type=request.json.get("media_type"),
+        watched_at=datetime.now(),
+        user_id=current_user_id,
+    )
+
+    db.session.add(want_watch)
+    db.session.commit()
+
+    watched_schema = WantWatchSchema()
+    result = watched_schema.dump(want_watch)
+
+    return jsonify(
+        success=True,
+        message="创建成功",
+        data=result,
+    ), 200
+
+
+@weapp_want_watched_bp.delete('<int:id>')
+@jwt_required()
+def destroy(id):
+    w = WantWatch.query.get(id)
+    if w is None:
+        return jsonify(
+            success=False,
+            message="资源不存在",
+        ), 404
+
+    db.session.delete(w)
+    db.session.commit()
+
+    return jsonify(
+        success=True,
+        message="删除成功"
+    )
