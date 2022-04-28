e78f4e5b979872b47fb80d4b35992bf915d65c2d
Seif Wu <seif.wuxf@gmail.com>
2022-04-28 15:52:43 +0800

    feat: 新增"想看"列表、详情接口

--- app/controllers/api/v1/weapp/want_watch.py
+++ app/controllers/api/v1/weapp/want_watch.py
@@ -10,6 +10,26 @@ from app.models.want_watch import WantWatch
 weapp_want_watched_bp = Blueprint(
     'want_watch', __name__, url_prefix='/want_watch')
 
+@weapp_want_watched_bp.get('')
+@jwt_required()
+def index():
+    args = request.args
+    want_watches = WantWatch.query.paginate(page=args.get(
+        "page", 1), per_page=args.get("pageSize", 10))
+    want_watche_schema = WantWatchSchema()
+    result = want_watche_schema.dump(want_watches.items, many=True)
+
+    return jsonify(
+        success=True,
+        date=result,
+        meta={
+            "has_next": want_watches.has_next,
+            "has_prev": want_watches.has_prev,
+            "page": want_watches.page,
+            "total": want_watches.total,
+        }
+    )
+
 
 @weapp_want_watched_bp.post('')
 @jwt_required()
@@ -63,4 +83,22 @@ def destroy(id):
     return jsonify(
         success=True,
         message="删除成功"
+    )
+
+@weapp_want_watched_bp.get('<int:id>')
+@jwt_required()
+def show(id):
+    w = WantWatch.query.get(id)
+    if w is None:
+        return jsonify(
+            success=False,
+            message="资源不存在",
+        ), 404
+
+    schema = WantWatchSchema()
+    result = schema.dump(w)
+
+    return jsonify(
+        success=True,
+        data=result
     )
