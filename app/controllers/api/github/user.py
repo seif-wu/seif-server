7fb4e77672768572898ae00d56baa7ac4471080c
Seif Wu <seif.wuxf@gmail.com>
2022-05-17 17:13:42 +0800

    feat: 添加 GitHub User 接口

--- app/controllers/api/github/user.py
+++ app/controllers/api/github/user.py
@@ -1,8 +1,17 @@
-from flask import Blueprint, jsonify, request
+import requests
+from flask import Blueprint
+from flask import jsonify
 
 github_user_bp = Blueprint('github_user_bp', __name__, url_prefix='/user')
 
 
 @github_user_bp.get('')
 def user():
-    pass
+    # TODO GitHub 用户名可配置
+    url = "https://api.github.com/users/seif-wu"
+    resp = requests.request("GET", url)
+
+    return jsonify({
+        "success": True,
+        "data": resp.json(),
+    })
