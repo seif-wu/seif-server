33deee9ec2e9025eb644262168785b31f0ba8e80
Seif Wu <seif.wuxf@gmail.com>
2022-05-21 22:23:59 +0800

    feat: GitHub Uer 添加 basic 认证

--- app/controllers/api/github/user.py
+++ app/controllers/api/github/user.py
@@ -1,3 +1,5 @@
+import os
+import base64
 import requests
 from flask import Blueprint
 from flask import jsonify
@@ -9,7 +11,15 @@ github_user_bp = Blueprint('github_user_bp', __name__, url_prefix='/user')
 def user():
     # TODO GitHub 用户名可配置
     url = "https://api.github.com/users/seif-wu"
-    resp = requests.request("GET", url)
+
+    basic_token = base64.b64encode(
+        bytes(f"seif-wu:{os.getenv('GITHUB_TOKEN')}", 'utf-8'))
+    headers = {
+        'Authorization': f"Basic {basic_token}",
+        'Content-Type': 'application/json'
+    }
+
+    resp = requests.request("GET", url, headers=headers)
 
     return jsonify({
         "success": True,
