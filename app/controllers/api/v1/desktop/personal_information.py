2c8c3187b08075a8f53ff01e48776d11f486eb1c
Seif Wu <seif.wuxf@gmail.com>
2022-05-31 15:35:20 +0800

    feat: 添加更新个人信息接口

--- app/controllers/api/v1/desktop/personal_information.py
+++ app/controllers/api/v1/desktop/personal_information.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required
+from app import db
 from app.utils.redis import Redis
 from app.models.personal_information import PersonalInformation
 from app.schema.personal_information_schema import PersonalInformationSchema
@@ -46,3 +48,53 @@ def show():
         success=True,
         data=result
     )
+
+
+@personal_information_bp.put('')
+@jwt_required()
+def update():
+    myself = PersonalInformation.myself()
+    if not myself:
+        myself = PersonalInformation
+
+    name = request.json.get("name", None)
+    mobile = request.json.get("mobile", None)
+    if not name:
+        return jsonify(success=False, message="名字不能为空")
+    if not mobile:
+        return jsonify(success=False, message="联系方式不能为空")
+
+    myself.name = name
+    myself.mobile = mobile
+
+    age = request.json.get("age", None)
+    if age is not None:
+        myself.age = age
+
+    email = request.json.get("email", None)
+    if email is not None:
+         myself.email = email
+
+    num_projects = request.json.get("num_projects", None)
+    if num_projects is not None:
+         myself.num_projects = num_projects
+
+    work_experience = request.json.get("work_experience", None)
+    if work_experience is not None:
+         myself.work_experience = work_experience
+
+    educational_background = request.json.get("educational_background", None)
+    if educational_background is not None:
+         myself.educational_background = educational_background
+
+    desc = request.json.get("desc", None)
+    if desc is not None:
+         myself.desc = desc
+
+    db.session.add(myself)
+    db.session.commit()
+
+    myself_schema = PersonalInformationSchema()
+    result = myself_schema.dump(myself)
+
+    return jsonify(success=True, data=result)
